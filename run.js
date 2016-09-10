238ba8f2cfc46d0ecd31fbd83a6706b0fca7d0d6
Kent Nguyen <nguyen.dmz@gmail.com>
2016-09-10 21:11:07 +0800

    Update logging

--- run.js
+++ run.js
@@ -6,13 +6,14 @@ miscale.startScanning();
 miscale.on('data', function (scale) {
   if (scale.isStabilized && !scale.loadRemoved) {
     // Don't need to print, push to Slack directly
-    //console.log(scale);
+    var txt = 'Detected ' + scale.weight + 'kg';
+    console.log(txt);
 
     request.post(
       'https://hooks.slack.com/services/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',
-      { json: { channel: '#tmp', username: 'Weight', icon_url: 'https://cdn2.iconfinder.com/data/icons/electronics-solid-icons-vol-2/48/084-512.png', text: 'Detected ' + scale.weight + 'kg'} },
+      { json: { channel: '#tmp', username: 'Weight', icon_url: 'https://cdn2.iconfinder.com/data/icons/electronics-solid-icons-vol-2/48/084-512.png', text: txt} },
       function (error, response, body) {
-        if (!error || response.statusCode != 200) {
+        if (error || response.statusCode != 200) {
           // Print only when there is error
           console.log(body);
         }
